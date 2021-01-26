328f859f2aac15a0993252215cf4dae002d136ab
vguenoff <vasil.genov@dhl.com>
2021-01-26 09:28:47 +0200

    Transform objects end

--- lessons/05-transforms-objects/src/script.js
+++ lessons/05-transforms-objects/src/script.js
@@ -1,19 +1,9 @@
 import './style.css'
 import * as THREE from 'three'
+import {Vector3} from 'three'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-/**
- * Objects
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 0xff0000})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
 /**
  * Sizes
@@ -23,18 +13,63 @@ const sizes = {
     height: 600,
 }
 
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0xff0000}),
+)
+
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00}),
+)
+cube2.position.x = -2
+
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff}),
+)
+cube3.position.x = 2
+
+group.add(cube3)
+
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(3)
+
+scene.add(axesHelper)
+
 /**
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.set(0, 0, 3)
+
 scene.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
+    canvas,
 })
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
