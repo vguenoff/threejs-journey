256f8eb8b0f1857760eb3166cd47dff6fc0ab6ee
vguenoff <vasil.genov@dhl.com>
2021-01-28 10:06:05 +0200

    Camera WIP

--- lessons/07-cameras/src/script.js
+++ lessons/07-cameras/src/script.js
@@ -1,5 +1,15 @@
 import './style.css'
-import * as THREE from 'three'
+import {
+    Scene,
+    Mesh,
+    BoxGeometry,
+    MeshBasicMaterial,
+    PerspectiveCamera,
+    WebGLRenderer,
+    Clock,
+    Vector3,
+    // OrthographicCamera,
+} from 'three'
 
 /**
  * Base
@@ -12,39 +22,73 @@ const sizes = {
     width: 800,
     height: 600,
 }
+const aspectRatio = sizes.width / sizes.height
+console.log({aspectRatio})
+
+/**
+ * Cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0,
+}
+
+window.addEventListener('mousemove', e => {
+    cursor.x = e.clientX / sizes.width - 0.5
+    cursor.y = -(e.clientY / sizes.height - 0.5)
+    // console.log(cursor)
+})
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new Scene()
 
 // Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({color: 0xff0000}),
+const mesh = new Mesh(
+    new BoxGeometry(1, 1, 1, 5, 5, 5),
+    new MeshBasicMaterial({color: 0xff0000}),
 )
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+// const camera = new OrthographicCamera(
+//     -1 * aspectRatio,
+//     1 * aspectRatio,
+//     1,
+//     -1,
+//     0.1,
+//     100,
+// )
+// camera.position.x = 2
+// camera.position.y = 2
+
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
+const renderer = new WebGLRenderer({
+    canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 
 // Animate
-const clock = new THREE.Clock()
+const clock = new Clock()
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime
+    // mesh.rotation.y = elapsedTime
+
+    // Update camera
+    // camera.position.x = cursor.x * 10
+    // camera.position.y = cursor.y * 10
+    // Full rotation
+    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    camera.position.y = cursor.y * 5
+    camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
